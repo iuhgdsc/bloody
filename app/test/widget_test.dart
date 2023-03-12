6dc6b3d50db7f99ade444a42324debf51e990d53
Hoa Xuân Sang <91694268+HXSang@users.noreply.github.com>
2023-03-12 12:47:47 +0700

    Update widget_test.dart

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -2,8 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  setUpAll(() async {
-    // Initialize Firebase for tests
-    await Firebase.initializeApp();
+    testWidgets('MyWidget has a title and message', (tester) async {
+    // Test code goes here.
   });
 }
