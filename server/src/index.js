a7af8af69d546deb6f0c7f18f7826004dc697750
longnguyen-cyber <longnguyendev2020@gmail.com>
2023-03-16 09:36:03 +0700

    redeploy

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 const cors = require('cors')
-
+require('./config/firestoreConfig')
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
